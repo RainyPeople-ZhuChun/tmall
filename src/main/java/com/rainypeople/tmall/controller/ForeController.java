1f526cd8c205bad31b15577a3a9dbe11ed3fb6e0
zhuchun <zhuchun@rainypeople.com>
2020-11-22 11:14:57 +0800

    完成商品页面信息展示

--- src/main/java/com/rainypeople/tmall/controller/ForeController.java
+++ src/main/java/com/rainypeople/tmall/controller/ForeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
@@ -89,7 +90,7 @@ public class ForeController {
 
         Category category = categoryService.getById(p.getCid());
         p.setCategory(category);
-        
+
         List<ProductImage> productSingleImages = productImageService.list(pid, productImageService.type_single);
         List<ProductImage> productDetailImages = productImageService.list(pid, productImageService.type_detail);
         p.setProductSingleImages(productSingleImages);
@@ -107,4 +108,27 @@ public class ForeController {
         return "fore/product";
     }
 
+    @RequestMapping("forecheckLogin")
+    @ResponseBody
+    public String checkLogin(HttpSession session){
+        User user = (User) session.getAttribute("user");
+        if (null==user){
+            return "fail";
+        }
+        return "success";
+    }
+
+    @RequestMapping("foreloginAjax")
+    @ResponseBody
+    public String loginAjax(@RequestParam("name")String name,
+                            @RequestParam("password")String password,
+                            HttpSession session){
+        name=HtmlUtils.htmlEscape(name);
+        User user = userService.get(name, password);
+        if (null==user){
+            return "fail";
+        }
+        session.setAttribute("user",user);
+        return "success";
+    }
 }
